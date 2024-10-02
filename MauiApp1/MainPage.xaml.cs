2a38aa321c3cea92a4942f3db54157ac54a8c9d4
sheymabel <122784710+sheymabel@users.noreply.github.com>
2024-10-02 01:42:19 +0100

    une petit example pour applique le style en xml c#

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAnimalClicked(object sender, EventArgs e)
+        {
+            // Navigate to the Animal page or show details
+        }
+
+        private void OnCalculRationClicked(object sender, EventArgs e)
+        {
+            // Navigate to the Calcul Ration page
+        }
+
+        private void OnConseilsClicked(object sender, EventArgs e)
+        {
+            // Show tips or navigate to the Conseils page
+        }
+
+        private void OnStatistiquesClicked(object sender, EventArgs e)
+        {
+            // Show statistics or navigate to the Statistiques page
+        }
+
+        private void OnFournisseursClicked(object sender, EventArgs e)
+        {
+            // Show suppliers or navigate to the Fournisseurs page
+        }
+
+        private void OnPrixMarcheClicked(object sender, EventArgs e)
+        {
+            // Show market prices or navigate to the Prix marché page
+        }
+
+
+    }
+
+}
